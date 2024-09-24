706b7970b5aea2c20dc20d85024755c3e11baa67
noguchi tomoki <noguchi.5tag@gmail.com>
2024-09-25 02:05:04 +0900

    更新

--- ngs-imagine/src/Layouts/Inner.vue
+++ ngs-imagine/src/Layouts/Inner.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue'
 </script>
 
 <template>
-    <div class="w-full lg:max-w-screen-lg lg:mx-auto px-6 lg:px-0">
+    <div class="w-full lg:max-w-screen-lg lg:mx-auto px-6 lg:px-0 relative">
         <slot />
     </div>
 </template>
--- ngs-imagine/src/components/Footer.vue
+++ ngs-imagine/src/components/Footer.vue
@@ -7,6 +7,11 @@ import 'swiper/css/navigation';
 import 'swiper/css';
 // Swiperのモジュールを指定
 const modules = [Navigation, A11y];
+
+const swiperNavigation = {
+    prevEl: '.custom-prev',
+    nextEl: '.custom-next',
+}
 </script>
 
 <template>
@@ -14,7 +19,8 @@ const modules = [Navigation, A11y];
     <Inner>
         <div class="my-10 lg:my-20">
             <div class="pb-10 lg:pb-20 border-b-gray-300 border-b-4">
-                <div class="footer-contents">
+                <div class="footer-contents relative">
+                    <button class="custom-prev bg-customBlue text-white py-1 lg:py-2 px-2.5 lg:px-4 rounded-full absolute top-1/2 -translate-y-1/2 -left-4 z-10 before:content-['<']"></button>
                     <swiper
                         :modules="modules"
                         :slides-per-view="2"
@@ -25,7 +31,7 @@ const modules = [Navigation, A11y];
                                 spaceBetween: 20
                             },
                         }"
-                        navigation
+                        :navigation="swiperNavigation"
                     >
                     <swiper-slide>
                         <a href="#">
@@ -68,6 +74,7 @@ const modules = [Navigation, A11y];
                         </a>
                     </swiper-slide>
                     </swiper>
+                    <button class="custom-next bg-customBlue text-white py-1 lg:py-2 px-2.5 lg:px-4 rounded-full absolute top-1/2 -translate-y-1/2 -right-4 z-10 before:content-['>']"></button>
                 </div>
             </div>
 
@@ -78,63 +85,37 @@ const modules = [Navigation, A11y];
                             <img class="w-full" src="../assets/images/logo/logo.svg" alt="長崎イマジンロゴ">
                         </div>
                         <div class="flex items-center justify-center gap-8 mt-6">
-                            <img class="w-6" src="../assets/images/sns/facebook.png" alt="facebook">
-                            <img class="w-6" src="../assets/images/sns/x.svg" alt="x">
-                            <img class="w-6" src="../assets/images/sns/instagram.png" alt="instagram">
+                            <img class="w-4 lg:w-6" src="../assets/images/sns/facebook.png" alt="facebook">
+                            <img class="w-4 lg:w-6" src="../assets/images/sns/x.svg" alt="x">
+                            <img class="w-4 lg:w-6" src="../assets/images/sns/instagram.png" alt="instagram">
                         </div>
                     </div>
-                    <div class="flex justify-around gap-8 mt-4 lg:mt-0">
+                    <div class="flex justify-around gap-4 lg:gap-8 mt-4 lg:mt-0">
                         <ul class="h-full flex flex-col justify-between lg:space-y-6">
-                            <li class="font-Maru font-bold text-base lg:text-lg lg:leading-loose leading-loose"><a href="#">長崎イマジンについて</a></li>
-                            <li class="font-Maru font-bold text-base lg:text-lg lg:leading-loose leading-loose"><a href="#">最新情報</a></li>
-                            <li class="font-Maru font-bold text-base lg:text-lg lg:leading-loose leading-loose"><a href="#">イマジントーーク！</a></li>
+                            <li class="font-Maru font-bold text-xs lg:text-lg lg:leading-loose leading-loose"><a href="#">長崎イマジンについて</a></li>
+                            <li class="font-Maru font-bold text-xs lg:text-lg lg:leading-loose leading-loose"><a href="#">最新情報</a></li>
+                            <li class="font-Maru font-bold text-xs lg:text-lg lg:leading-loose leading-loose"><a href="#">イマジントーーク！</a></li>
                         </ul>
                         <ul class="lg:flex items-end justify-between gap-8">
                             <div class="lg:space-y-6">
-                                <li class="font-Maru font-bold text-base lg:text-lg lg:leading-loose leading-loose"><a href="#">特集</a></li>
-                                <li class="list-disc font-Maru text-base lg:leading-loose leading-loose"><a href="#">長崎今人 ながさき昭和100年史</a></li>
-                                <li class="list-disc font-Maru text-base lg:leading-loose leading-loose"><a href="#">アマフェッショナル散策の流儀</a></li>
+                                <li class="font-Maru font-bold text-xs lg:text-lg lg:leading-loose leading-loose"><a href="#">特集</a></li>
+                                <li class="list-disc font-Maru text-xs lg:leading-loose leading-loose"><a href="#">長崎今人 ながさき昭和100年史</a></li>
+                                <li class="list-disc font-Maru text-xs lg:leading-loose leading-loose"><a href="#">アマフェッショナル散策の流儀</a></li>
                             </div>
                             <div class="lg:space-y-6">
-                                <li class="list-disc font-Maru text-base lg:leading-loose leading-loose"><a href="#">長崎ひまじん</a></li>
-                                <li class="list-disc font-Maru text-base lg:leading-loose leading-loose"><a href="#">長崎今走る人イマRUNジン </a></li>
+                                <li class="list-disc font-Maru text-xs lg:leading-loose leading-loose"><a href="#">長崎ひまじん</a></li>
+                                <li class="list-disc font-Maru text-xs lg:leading-loose leading-loose"><a href="#">長崎今走る人イマRUNジン </a></li>
                             </div>
                         </ul>
                     </div>
                 </div>
             </div>
-            <p class="text-center mt-4">&copy;長崎イマジン</p>
+            <p class="text-center mt-4 text-[10px]">&copy;長崎イマジン</p>
         </div>
     </Inner>
 </template>
 
 <style>
-.footer-contents .swiper-button-prev,
-.footer-contents .swiper-button-next {
-    background-color: #354896;
-    padding: 4px;
-    border-radius: 50%;
-    color: white;
-    width: 40px;
-    height: 40px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-    top: 50%;
-    transform: translateY(0%);
-    z-index: 10;
-}
-@media (min-width: 1024px) {
-    .footer-contents .swiper-button-prev,
-    .footer-contents .swiper-button-next {
-        padding: 10px;
-        width: 40px;
-        height: 40px;
-        top: 50%;
-    }
-}
-
 .footer-contents .swiper-button-prev:after,
 .footer-contents .swiper-button-next:after {
     font-size:medium;
